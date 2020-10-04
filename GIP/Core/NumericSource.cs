5d0ab5107b2be11aa2a5832b4a945dcc306ae14c
RT-EGG <rt.egg.jp@gmail.com>
2020-10-04 17:30:57 +0900

    Devide numeric source implimentation.

---
+++ GIP/Core/NumericSource.cs
@@ -0,0 +1,61 @@
+﻿namespace GIP.Core
+{
+    public interface INumericSource
+    {
+
+    }
+
+    public interface IIntSource : INumericSource
+    {
+        int Value { get; }
+    }
+
+    public interface IFloatSource : INumericSource
+    {
+        float Value { get; }
+    }
+
+    public class IntSource : IIntSource
+    {
+        public IntSource(int inValue = default)
+        {
+            Value = inValue;
+            return;
+        }
+
+        public int Value
+        { get; set; } = 0;
+
+        public static implicit operator int(IntSource inValue)
+        {
+            return inValue.Value;
+        }
+
+        public static implicit operator IntSource(int inValue)
+        {
+            return new IntSource(inValue);
+        }
+    }
+
+    public class FloatSource : IFloatSource
+    {
+        public FloatSource(float inValue = default)
+        {
+            Value = inValue;
+            return;
+        }
+
+        public float Value
+        { get; set; } = 0.0f;
+
+        public static implicit operator float(FloatSource inValue)
+        {
+            return inValue.Value;
+        }
+
+        public static implicit operator FloatSource(float inValue)
+        {
+            return new FloatSource(inValue);
+        }
+    }
+}
